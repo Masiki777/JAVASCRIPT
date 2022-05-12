e76d4883cb1f515114c864fdbe373db856639bbb
RAJABU R MASIKI <102743823+Masiki777@users.noreply.github.com>
2022-05-12 16:22:38 +0300

    setInterval, clearInterval, Animations

---
+++ index.js
@@ -0,0 +1,12 @@
+const circle = document.getElementById('circle');
+const xFinalPosition = 900;
+let xPos = 0;
+
+const interval = window.setInterval(()=>{
+    if(xPos > xFinalPosition){
+        return window.clearInterval(interval)
+    }
+    xPos += 1
+    circle.style.transform = `translateX(${xPos}px)`
+
+}, 1);
